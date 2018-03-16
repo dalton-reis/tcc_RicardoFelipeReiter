cceb591555d3c630e53431c1905df9d672f893c6
ricardoreiter <ricardof.reiter@gmail.com>
2018-03-16 02:28:28 -0300

    CubeMarker timer to get object

--- AnimAR/Assets/Scripts/CubeMarkerController.cs
+++ AnimAR/Assets/Scripts/CubeMarkerController.cs
@@ -8,11 +8,40 @@ namespace Assets.Scripts {
     public class CubeMarkerController : MonoBehaviour {
 
         public CubeMarkerIndicatorController indicatorController;
+        private const float secondsToHold = 2;
+        private const float movementTolerance = 0.16f;
 
         private GameObject scene = null;
         private GameObject objectMarkerOver = null;
         private GameObject attachedObject = null;
         private CubeMarkerStatus currentStatus = CubeMarkerStatus.NOP;
+
+        private float currentTime = 0;
+        private Vector3 lastPos = Vector3.zero;
+
+        void FixedUpdate() {
+            if (currentStatus == CubeMarkerStatus.MARKER_OVER_OBJECT || currentStatus == CubeMarkerStatus.OBJECT_ATTACHED) {
+                if (Math.Abs(Vector3.Distance(lastPos, transform.position)) > movementTolerance) {
+                    currentTime = secondsToHold;
+                } else {
+                    currentTime -= Time.fixedDeltaTime;
+                    if (currentTime <= 0) {
+                        if (objectMarkerOver) {
+                            objectMarkerOver.transform.parent = this.transform;
+                            attachedObject = objectMarkerOver;
+                            objectMarkerOver = null;
+                            UpdateStatus();
+                        } else if (attachedObject && scene) {
+                            attachedObject.transform.parent = scene.transform;
+                            attachedObject = null;
+                            UpdateStatus();
+                        }
+                        currentTime = secondsToHold;
+                    }
+                }
+            }
+                lastPos = transform.position;
+        }
 
         void OnTriggerEnter(Collider other) {
             Debug.Log(other);
@@ -22,6 +51,7 @@ namespace Assets.Scripts {
                     break;
                 case "Movable":
                     objectMarkerOver = other.gameObject;
+                    currentTime = secondsToHold;
                     break;
             }
             UpdateStatus();
