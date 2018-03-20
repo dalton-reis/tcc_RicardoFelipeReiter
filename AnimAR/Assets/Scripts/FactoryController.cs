dca86c4b5093ffe8ec46940a2d40e7a4e8cd8839
ricardoreiter <ricardof.reiter@gmail.com>
2018-03-19 23:38:39 -0300

    Object factory

---
+++ AnimAR/Assets/Scripts/FactoryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Vuforia;
+
+namespace Assets.Scripts {
+    public class FactoryController : MonoBehaviour, IVirtualButtonEventHandler {
+
+        public VirtualButtonBehaviour NextButton;
+        public VirtualButtonBehaviour PrevButton;
+        public GameObject[] objList;
+
+        private int currentObjIndex = 0;
+        private GameObject currentObject = null;
+
+        void Start() {
+            NextButton.RegisterEventHandler(this);
+            PrevButton.RegisterEventHandler(this);
+        }
+
+        public void OnButtonPressed(VirtualButtonBehaviour vb) {
+            switch (vb.VirtualButtonName) {
+                case "Next":
+                    ChangeIndex(currentObjIndex + 1);
+                    break;
+                case "Prev":
+                    ChangeIndex(currentObjIndex - 1);
+                    break;
+            }
+        }
+
+        public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        }
+
+        private void ChangeIndex(int index) {
+            currentObjIndex = Math.Abs(index % objList.Length);
+            if (currentObject) {
+                Destroy(currentObject);
+            }
+            currentObject = GameObject.Instantiate(objList[currentObjIndex]);
+            currentObject.transform.parent = this.transform;
+            currentObject.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+    }
+}
