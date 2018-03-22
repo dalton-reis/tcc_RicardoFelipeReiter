ddb293dd391a3c08fd54d5fe942e12c1050e243b
ricardoreiter <ricardof.reiter@gmail.com>
2018-03-21 23:48:16 -0300

    add button press animation

---
+++ AnimAR/Assets/Scripts/VirtualButtonAnimationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Vuforia;
+
+namespace Assets.Scripts {
+    public class VirtualButtonAnimationController : MonoBehaviour, IVirtualButtonEventHandler {
+
+        public Material standyByMaterial;
+        public Material overMaterial;
+        public GameObject VirtualButton;
+
+        void Start() {
+            VirtualButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+            GetComponent<MeshRenderer>().material = standyByMaterial;
+        }
+
+        public void OnButtonPressed(VirtualButtonBehaviour vb) {
+ 	        GetComponent<MeshRenderer>().material = overMaterial;
+        }
+
+        public void OnButtonReleased(VirtualButtonBehaviour vb) {
+ 	        GetComponent<MeshRenderer>().material = standyByMaterial;
+        }
+    }
+}
