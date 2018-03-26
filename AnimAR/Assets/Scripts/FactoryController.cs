4a98a2333e4b9f70e85f50867d34b2a465bd71e4
ricardoreiter <ricardof.reiter@gmail.com>
2018-03-26 00:53:28 -0300

    Fix object factory

--- AnimAR/Assets/Scripts/FactoryController.cs
+++ AnimAR/Assets/Scripts/FactoryController.cs
@@ -19,9 +19,6 @@ namespace Assets.Scripts {
         void Start() {
             NextButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
             PrevButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-            Debug.Log("lolol");
-            Debug.Log(PrevButton);
-            Debug.Log(NextButton);
             ChangeIndex(0);
         }
 
