796de54a1f4315c564882f4cc863e46d118ea395
ricardoreiter <ricardof.reiter@gmail.com>
2018-04-27 19:10:42 -0300

    menu principal

---
+++ AnimAR/Assets/Scripts/ChangeVuforiaConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Vuforia;
+
+namespace Assets.Scripts {
+    public class ChangeVuforiaConfig : MonoBehaviour {
+
+        void Start() {
+            var vuforiaType = PlayerPrefs.GetString("vuforiaType");
+            if (vuforiaType.Equals("vr")) {
+                Debug.Log("LOLLO");
+                DigitalEyewearARController.Instance.SetEyewearType(DigitalEyewearARController.EyewearType.VideoSeeThrough);
+                DigitalEyewearARController.Instance.SetStereoCameraConfiguration(DigitalEyewearARController.StereoFramework.Cardboard);
+            } else {
+                DigitalEyewearARController.Instance.SetEyewearType(DigitalEyewearARController.EyewearType.None);
+            }
+        }
+    }
+}
---
+++ AnimAR/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+namespace Assets.Scripts {
+    public class MainMenuController : MonoBehaviour {
+
+        public Toggle CardboardToggle;
+
+        void Start() {
+            var vuforiaType = PlayerPrefs.GetString("vuforiaType");
+            CardboardToggle.isOn = vuforiaType.Equals("vr");
+        }
+
+        public void LoadVuforiaScene() {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+
+        public void CardboardToggleChanged(Boolean value) {
+            if (CardboardToggle.isOn) {
+                PlayerPrefs.SetString("vuforiaType", "vr");
+            } else {
+                PlayerPrefs.SetString("vuforiaType", "non-vr");
+            }
+        }
+        public void StartWithoutVR() {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+
+        public void ShowTutorial() {
+
+        }
+
+        public void ShowAbout() {
+
+        }
+
+        public void CleanSavedData() {
+
+        }
+
+        public void Exit() {
+            Application.Quit();
+        }
+
+    }
+}
